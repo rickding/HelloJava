c2efd7ab40195ffd4f16d6ea9243324ded4ae54c
DING Xianli <ylpdxl@hotmail.com>
2017-05-27 16:44:40 +0800

    Spring MVC, worked

---
+++ HelloSpringMvc/README.md
@@ -0,0 +1,6 @@
+# springmvc
+最最简单的springmvc例子
+
+启动后可通过访问http://localhost:8080/springmvc/index查看效果
+
+如果显示Spring Demo Success!字样，则表示成功
---
+++ HelloSpringMvc/src/main/java/com/springdemo/web/DemoController.java
@@ -0,0 +1,13 @@
+package com.springdemo.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class DemoController {
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String index() {
+        return "index";
+    }
+}
