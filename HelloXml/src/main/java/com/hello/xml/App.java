bcf1f4fed683ed05d50e386782dd084669ad2890
DING Xianli <ylpdxl@hotmail.com>
2017-05-27 11:55:12 +0800

    Hello DOM4J, StAX, XmlMapper

--- HelloXml/src/main/java/com/hello/xml/App.java
+++ HelloXml/src/main/java/com/hello/xml/App.java
@@ -1,97 +1,37 @@
 package com.hello.xml;
 
-import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import org.dom4j.DocumentException;
 
 import javax.xml.stream.*;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.StringWriter;
+import java.io.*;
 
 /**
  * Created by user on 2017/5/26.
  */
 public class App {
     public static void main(String[] args) {
-        ObjectMapper xmlMapper = new XmlMapper();
+        HelloXmlMapper.mapToString();
+        HelloXmlMapper.fromString("<Simple><x>3</x><y>4</y></Simple>");
 
+        HelloStAX.mapToString();
+        HelloStAX.fromString();
+
+        // dom4j, string
         try {
-            String xml = xmlMapper.writeValueAsString(new Simple());
-            System.out.println(xml);
-        } catch (JsonProcessingException e) {
+            HelloDOM4j.fromString("<Simple><x>5</x><y>6</y></Simple>");
+        } catch (DocumentException e) {
             e.printStackTrace();
         }
 
+        // dom4j, saxReader, inputstream, file
         try {
-            xmlMapper.writeValue(new File("x.xml"), new Simple());
-        } catch (IOException e) {
+            HelloDOM4j.fromFile("C:\\Work\\x.xml");
+        } catch (DocumentException e) {
             e.printStackTrace();
-        }
-
-        try {
-            Simple v = xmlMapper.readValue("<Simple><x>3</x><y>4</y></Simple>", Simple.class);
-            System.out.println(v);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        // Create xml
-        XMLInputFactory xmlInputFactory = XMLInputFactory.newFactory();
-        XMLOutputFactory xmlOutputFactory = XMLOutputFactory.newFactory();
-
-        try {
-            StringWriter out = new StringWriter();
-            XMLStreamWriter sw = xmlOutputFactory.createXMLStreamWriter(out);
-
-            XmlMapper mapper = new XmlMapper(xmlInputFactory);
-            sw.writeStartDocument();
-            ;
-            sw.writeStartElement("root");
-
-            Simple s1 = new Simple();
-            mapper.writeValue(sw, s1);
-            s1.setX(3);
-            mapper.writeValue(sw, s1);
-
-            sw.writeComment("comment some");
-            sw.writeEndElement();
-            sw.writeEndDocument();
-
-            sw.flush();
-            sw.close();
-
-            System.out.println(out.toString());
-        } catch (XMLStreamException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            // Parse xml
-            XMLInputFactory f = XMLInputFactory.newFactory();
-            File inputFile = new File("C:\\Work\\x.xml");
-            XMLStreamReader sr = f.createXMLStreamReader(new FileInputStream(inputFile));
-
-            XmlMapper mapper = new XmlMapper();
-            sr.next();
-
-            while (sr.hasNext()) {
-                sr.next();
-                if (!sr.isStartElement())
-                    continue;
-
-                Simple s1 = mapper.readValue(sr, Simple.class);
-                System.out.println(s1);
-            }
-
-            sr.close();
-        } catch (XMLStreamException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
     }
