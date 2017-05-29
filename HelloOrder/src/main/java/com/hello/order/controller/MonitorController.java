63416926567b583e77fb1436f3e88b660fd31be9
DING Xianli <ylpdxl@hotmail.com>
2017-05-29 18:59:45 +0800

    Update version, spring 3.2.0, quartz 1.8.5

--- HelloOrder/src/main/java/com/hello/order/controller/MonitorController.java
+++ HelloOrder/src/main/java/com/hello/order/controller/MonitorController.java
@@ -3,15 +3,15 @@ package com.hello.order.controller;
 import com.hello.order.job.ChannelOrderSyncJob;
 import com.hello.util.DateUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by user on 2017/5/29.
  */
 
-@RestController
+@Controller
 public class MonitorController {
     @Autowired
     private ChannelOrderSyncJob channelOrderSyncJob;
