c346b202139edb86b25902a2ab6fa5b83c4c3225
dingxl <dingxl18@qq.com>
2018-12-10 15:44:36 +0800

    update

--- HelloWebCustomer/src/main/java/com/hello/web/customer/WebCustomerController.java
+++ HelloWebCustomer/src/main/java/com/hello/web/customer/WebCustomerController.java
@@ -1,17 +1,16 @@
 package com.hello.web.customer;
 
 import com.hello.web.WebController;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Map;
 
 @Controller
-public class WebCustomerController {
-    @RequestMapping("/")
-    public String index(Map<String, Object> dataMap) {
-        dataMap.put("str", "WebCustomerController.index");
-        return "/index";
-    }
+public class WebCustomerController extends WebController{
+//    @RequestMapping("/")
+//    public String index(Map<String, Object> dataMap) {
+//        dataMap.put("str", "WebCustomerController.index");
+//        return "/index";
+//    }
 }
