316d5be1fc4bb3f83537d16abb3e26d86b6fdb62
dingxl <dingxl18@qq.com>
2018-12-10 15:25:35 +0800

    web customer

---
+++ HelloWebCustomer/src/main/java/com/hello/web/customer/WebCustomerApplication.java
@@ -0,0 +1,11 @@
+package com.hello.web.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCustomerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebCustomerApplication.class, args);
+    }
+}
