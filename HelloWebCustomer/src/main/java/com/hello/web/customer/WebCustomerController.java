87192832899da41bd11d66d287954ed5c5c1f9d0
dingxl <dingxl@dds.com.cn>
2018-12-12 09:21:50 +0800

    Extends webController

--- HelloWebCustomer/src/main/java/com/hello/web/customer/WebCustomerController.java
+++ HelloWebCustomer/src/main/java/com/hello/web/customer/WebCustomerController.java
@@ -1,15 +1,22 @@
 package com.hello.web.customer;
 
+import com.hello.web.WebController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Map;
 
 @Controller
-public class WebCustomerController {
+public class WebCustomerController extends WebController {
     @RequestMapping("/con_customer")
     public String con_customer(Map<String, Object> dataMap) {
         dataMap.put("str", "WebCustomerController.con_customer");
         return "/con_customer";
     }
+
+//    @RequestMapping("/con")
+//    public String con(Map<String, Object> dataMap) {
+//        dataMap.put("str", "WebCustomerController.con");
+//        return "/con";
+//    }
 }
