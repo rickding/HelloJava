f529491249d3424942ff93558d2ad8e068850a1b
dingxl <dingxl18@qq.com>
2018-12-26 17:06:37 +0800

    move WEB_INF, rename war

--- HelloWebWar/src/main/java/com/hello/web/WebController.java
+++ HelloWebWar/src/main/java/com/hello/web/WebController.java
@@ -7,14 +7,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.Map;
 
 @Controller
-@RequestMapping("/")
 public class WebController {
     @Autowired
     WebAction webAction;
 
-    @RequestMapping("/ctrl")
+    @RequestMapping("ctrl")
     public String ctrl(Map<String, Object> dataMap) {
         dataMap.put("str", this.getClass().getName() + ".ctrl, " + webAction.action());
-        return "/ctrl";
+        return "ctrl";
     }
 }
