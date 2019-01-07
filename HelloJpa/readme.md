c1c030800f28a021a52d8a62bf76e2495dc01bb2
dingxl <dingxl@dds.com.cn>
2019-01-07 14:55:19 +0800

    hellojpa

---
+++ HelloJpa/readme.md
@@ -0,0 +1,2 @@
+# jpa
+https://www.jianshu.com/p/c14640b63653
---
+++ HelloJpa/src/main/java/com/hello/jpa/model/UserDO.java
@@ -0,0 +1,54 @@
+package com.hello.jpa.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "auth_user")
+public class UserDO {
+    @Id
+    Long id;
+
+    @Column(length = 32)
+    String name;
+
+    @Column(length = 32)
+    String account;
+
+    @Column(length = 64)
+    String pwd;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+}
