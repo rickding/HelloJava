330e8464baf7dc4cb8b5ecc53de2a082ba5abf95
dingxl <dingxl@dds.com.cn>
2019-01-07 17:01:24 +0800

    validtor

--- HelloJpa/readme.md
+++ HelloJpa/readme.md
@@ -1,2 +1,5 @@
 # jpa
 https://www.jianshu.com/p/c14640b63653
+
+# validator
+https://www.cnblogs.com/mr-yang-localhost/p/7812038.html
--- HelloJpa/src/main/java/com/hello/jpa/JpaController.java
+++ HelloJpa/src/main/java/com/hello/jpa/JpaController.java
@@ -1,6 +1,9 @@
 package com.hello.jpa;
 
+import com.hello.jpa.dao.UserDO;
+import com.hello.jpa.dao.UserDao;
 import com.hello.jpa.validator.UserModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +16,9 @@ import java.util.logging.Logger;
 @RestController
 public class JpaController {
     Logger log = Logger.getLogger(JpaController.class.getName());
+
+    @Autowired
+    UserDao userDao;
 
     @RequestMapping("/add")
     public String add(@RequestBody @Valid UserModel userModel, BindingResult result) {
@@ -27,6 +33,16 @@ public class JpaController {
 
             return sb.substring(2);
         }
-        return "ok";
+
+        UserDO userDO = new UserDO(){{
+            setId(userDao.count() + 1);
+            setName(userModel.getName());
+            setAccount(userModel.getAccount());
+            setPwd(userModel.getPwd());
+
+        }};
+
+        userDao.save(userDO);
+        return String.format("ok, %d", userDao.count());
     }
 }
