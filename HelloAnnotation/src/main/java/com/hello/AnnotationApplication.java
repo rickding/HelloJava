8bea9b42781ad77da71e4dbd55116dc27cbdbdee
dingxl <dingxl>
2020-01-01 20:12:03 +0800

    db mapperScan

--- HelloAnnotation/src/main/java/com/hello/AnnotationApplication.java
+++ HelloAnnotation/src/main/java/com/hello/AnnotationApplication.java
@@ -1,5 +1,6 @@
 package com.hello;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -8,6 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableAsync
 @EnableScheduling
 @SpringBootApplication
+@MapperScan("com.hello.mapper")
 public class AnnotationApplication {
     public static void main(String[] args) {
         SpringApplication.run(AnnotationApplication.class, args);
---
+++ HelloAnnotation/src/main/java/com/hello/mapper/LogMapper.java
@@ -0,0 +1,7 @@
+package com.hello.mapper;
+
+import java.util.List;
+
+public interface LogMapper {
+    List<Object> selectAll();
+}
