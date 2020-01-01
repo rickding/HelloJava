f455cd527202ace11547abf857a448ae450d33fe
dingxl <dingxl>
2020-01-01 19:19:06 +0800

    restControllerAdvice

--- HelloAnnotation/src/main/java/com/hello/controller/ExceptionController.java
+++ HelloAnnotation/src/main/java/com/hello/controller/ExceptionController.java
@@ -2,14 +2,10 @@ package com.hello.controller;
 
 import com.hello.interceptor.AccessLimitException;
 import com.hello.util.RespUtil;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import javax.servlet.http.HttpServletRequest;
-
-@ControllerAdvice
-@RestController
+@RestControllerAdvice
 public class ExceptionController {
     @ExceptionHandler(value = AccessLimitException.class)
     public Object accessLimitExceptionHandler(AccessLimitException e) {
