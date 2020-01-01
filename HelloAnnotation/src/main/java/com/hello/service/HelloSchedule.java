0b15bbdaf07f3001e6217fc1eacba657799a7391
dingxl <dingxl>
2020-01-01 14:36:01 +0800

    scheduled

--- HelloAnnotation/src/main/java/com/hello/service/HelloSchedule.java
+++ HelloAnnotation/src/main/java/com/hello/service/HelloSchedule.java
@@ -7,17 +7,18 @@ import org.springframework.stereotype.Service;
 @Service
 @Async
 public class HelloSchedule {
-    @Scheduled(cron = "0/20 * * * * *")
+    @Async
+    @Scheduled(cron = "0/5 * * * * *")
     public void scheduled1() {
         System.out.println(String.format("cron %d", System.currentTimeMillis()));
     }
 
-    @Scheduled(fixedRate = 1000 * 20)
+    @Scheduled(fixedRate = 1000 * 7)
     public void scheduled2() {
         System.out.println(String.format("fixedRate %d", System.currentTimeMillis()));
     }
 
-    @Scheduled(fixedDelay = 1000 * 30)
+    @Scheduled(fixedDelay = 1000 * 11)
     public void scheduled3() {
         System.out.println(String.format("fixedDelay %d", System.currentTimeMillis()));
     }
