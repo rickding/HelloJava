1a127254b3fe35a592734bd2affdafeaef267947
dingxl <dingxl>
2020-01-01 19:29:25 +0800

    ResponseStatus

--- HelloAnnotation/src/main/java/com/hello/controller/HelloController.java
+++ HelloAnnotation/src/main/java/com/hello/controller/HelloController.java
@@ -4,11 +4,13 @@ import com.hello.annotation.AccessLimited;
 import com.hello.annotation.ClientIP;
 import com.hello.service.HelloBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -20,6 +22,7 @@ public class HelloController {
     @Autowired
     HelloBean helloBean;
 
+    @ResponseStatus(HttpStatus.ACCEPTED)
     @AccessLimited(count = 1)
     @RequestMapping(path="hello/{name}", method = {RequestMethod.GET, RequestMethod.POST})
     public Object hello(@ClientIP String ip, @PathVariable String name, @RequestParam String gender) {
