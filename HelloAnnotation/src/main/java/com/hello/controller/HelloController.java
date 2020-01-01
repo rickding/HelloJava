ff110622acf1057cf2cb9aebfd5786aa9593abf5
dingxl <dingxl>
2020-01-01 18:41:48 +0800

    cross origin

--- HelloAnnotation/src/main/java/com/hello/controller/HelloController.java
+++ HelloAnnotation/src/main/java/com/hello/controller/HelloController.java
@@ -4,6 +4,7 @@ import com.hello.annotation.AccessLimited;
 import com.hello.annotation.ClientIP;
 import com.hello.service.HelloBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,6 +18,7 @@ public class HelloController {
     @Autowired
     HelloBean helloBean;
 
+    @CrossOrigin("http://jext.top")
     @AccessLimited(count = 1)
     @RequestMapping(path="hello/{name}", method = {RequestMethod.GET, RequestMethod.POST})
     public Object hello(@PathVariable String name, @ClientIP String ip) {
