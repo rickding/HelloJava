2e574dc4a8ff6d2808475709c70a1b4b39279853
dingxl <dingxl>
2020-01-02 11:11:56 +0800

    set response status

--- HelloAnnotation/src/main/java/com/hello/controller/ExceptionController.java
+++ HelloAnnotation/src/main/java/com/hello/controller/ExceptionController.java
@@ -2,16 +2,20 @@ package com.hello.controller;
 
 import com.hello.exception.AccessLimitException;
 import com.hello.util.RespUtil;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ExceptionController {
+    @ResponseStatus(HttpStatus.TOO_MANY_REQUESTS)
     @ExceptionHandler(value = AccessLimitException.class)
     public Object accessLimitExceptionHandler(AccessLimitException e) {
         return RespUtil.resp(-100, e.getMessage());
     }
 
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(value = Exception.class)
     public Object exceptionHandler(Exception e) {
         return RespUtil.resp(-1, e.getMessage());
