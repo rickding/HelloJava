6cc046b5e9d88adfc270ad1d2d70e81c69df9f15
dingxl <dingxl>
2020-01-09 10:25:19 +0800

    shiro security

---
+++ HelloStarter/src/main/java/com/hello/config/ShiroConfig.java
@@ -0,0 +1,50 @@
+package com.hello.config;
+
+import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
+import org.apache.shiro.crypto.hash.Sha256Hash;
+import org.apache.shiro.mgt.SessionsSecurityManager;
+import org.apache.shiro.realm.Realm;
+import org.apache.shiro.spring.web.config.DefaultShiroFilterChainDefinition;
+import org.apache.shiro.spring.web.config.ShiroFilterChainDefinition;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ShiroConfig {
+    @Bean
+    public ShiroFilterChainDefinition shiroFilterChainDefinition() {
+        DefaultShiroFilterChainDefinition chainDefinition = new DefaultShiroFilterChainDefinition();
+
+        chainDefinition.addPathDefinition("/login", "anon");
+        chainDefinition.addPathDefinition("/chk/**", "authc, roles[admin]");
+        chainDefinition.addPathDefinition("/api/**", "anon");
+
+        // all other paths require a logged in user
+        chainDefinition.addPathDefinition("/**", "authc");
+        return chainDefinition;
+    }
+
+    @Bean
+    public SessionsSecurityManager securityManager() {
+        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
+        securityManager.setRealm(userRealm());
+        return securityManager;
+    }
+
+    @Bean
+    public Realm userRealm() {
+        UserRealm userRealm = new UserRealm();
+        userRealm.setCredentialsMatcher(credentialsMatcher());
+        return userRealm;
+    }
+
+    @Bean
+    public HashedCredentialsMatcher credentialsMatcher() {
+        HashedCredentialsMatcher credentialsMatcher = new HashedCredentialsMatcher();
+        credentialsMatcher.setHashAlgorithmName(Sha256Hash.ALGORITHM_NAME);
+        credentialsMatcher.setStoredCredentialsHexEncoded(false);
+        credentialsMatcher.setHashIterations(1024);
+        return credentialsMatcher;
+    }
+}
